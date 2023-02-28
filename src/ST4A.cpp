a7a08f1b20355fec31ca896ab3afb4add97c8eed
YUKICHI6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-03-01 04:04:20 +0900

    INFOを改善

--- src/ST4A.cpp
+++ src/ST4A.cpp
@@ -4,6 +4,8 @@
 #include "can_utils.hpp"
 #include<math.h>
 
+//初回必ず何か操作しないといけない欠陥仕様
+
 sensor_msgs::Joy gmsg;
 
 int gcount = 0;
@@ -32,6 +34,7 @@ int main(int argc, char **argv){
     ros::Rate loop_rate(10);
 
     int count = 0;
+    int pi_count = 0;
     while (ros::ok()){
         ROS_INFO("Tomo no sonshitu: 30000yen otu");
         if(gcount == 1)
@@ -49,13 +52,12 @@ int main(int argc, char **argv){
                 chatter.publish(get_frame(0x300,static_cast<uint8_t>(1)));
             }
             //↑Aボタンで手動でmodeをdisableへ
-            int pi_count = 0;
             if(msg.buttons[4]==1)
             {
                 pi_count--;
             }
             //ZLボタンで減少
-            if(msg.buttons[5])
+            if(msg.buttons[5]==1)
             {
                 pi_count++;
             }
@@ -68,9 +70,11 @@ int main(int argc, char **argv){
             if(msg.buttons[0]==1)
             {
                 chatter.publish(get_frame(0x301, 3.14f*(pi_count)));
+                ROS_INFO("chat");
             }
             //Xボタンで実行
             //can_plugins::Frame a = get_frame(0x101, 1.0f);
+            ROS_INFO("%d",pi_count);
         }
 
         ros::spinOnce();
